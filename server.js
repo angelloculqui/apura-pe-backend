85c38373cf57da87fd79736631a6d568fe5a5db5
angelloculqui <121901853+angelloculqui@users.noreply.github.com>
2025-08-11 19:30:18 -0500

    Create server.js

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const cors = require("cors");
+const routes = require("./src/routes/index");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.use("/", routes);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Servidor backend APURA.PE corriendo en puerto ${PORT}`);
+});
